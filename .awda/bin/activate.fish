00671a414e139692709dab1caadeb89019d5b5ea
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-01-25 07:40:23 +0200

    update

--- .awda/bin/activate.fish
+++ .awda/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/assem/Documents/Projects/awda/.awda
+set -gx VIRTUAL_ENV /home/assem/Github/awda/.awda
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
